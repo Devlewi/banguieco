1733b45fd92ba44d77a3540a7a1e3effebd7832e
devlewi <lewimarcjean3@gmail.com>
2026-04-20 10:57:43 +0000

    first commit

---
+++ src/app/[locale]/metadata.tsx
@@ -0,0 +1,12 @@
+// metadata.tsx
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BANGUI ECO | ACCUEIL",
+  description:
+    "Découvrez l'actualité économique en République Centrafricaine",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
